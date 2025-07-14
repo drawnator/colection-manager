2941725bcb3f290a84a8a67b612ffd48d8a153ac
Guilherme.Toledo <guilherme.toledo@ccc.ufcg.edu.br>
2025-07-13 22:11:22 -0300

    [add] user crud base

---
+++ src/models/User.ts
@@ -0,0 +1,52 @@
+import { Model, DataTypes, Optional } from 'sequelize';
+import sequelize from '../config/database';
+
+
+// Defina os atributos do modelo
+interface UserAttributes {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+}
+
+//professor esqueceu de adicionar essa linha importante para o código funcionar
+type UserCreationAttributes = Optional<UserAttributes, 'id'>;
+
+
+export class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
+  public id!: number;
+  public name!: string;
+  public email!: string;
+  public password!: string;
+}
+
+
+// Inicialize o modelo com os campos no banco
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "users", 
+    timestamps: false,
+  }
+);
